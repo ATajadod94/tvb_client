1c8c517072e3d7cef72b32c3b4d696d38a870ce7
Ali <atajadod94@gmail.com>
2018-08-10 13:21:04 -0400

    switching to wxpython for now

--- resting_state.py
+++ resting_state.py
@@ -7,5 +7,9 @@ from modelling_resting_state import *
 x = resting_state('resources/Node7[Public]/resting_state_hands_on/connectivity_HagmannDeco66.zip', 'resources/Node7[Public]/resting_state_hands_on/Hagmann_empFC_avg.npy')
 
 
-x.visualize()
+model = mymodel()
+model.local_excitatory()
+model.phase_flow()
+
+
 
